8de8d2bf2e923335b4ab21019c5caa67e3100bd6
taoyage <tougakaku@gmail.com>
2016-10-19 15:03:06 +0900

    添加平台检测

--- www/js/app.js
+++ www/js/app.js
@@ -14,25 +14,25 @@ if (window.StatusBar) {
       StatusBar.styleDefault();
     }
 
-    // //平台、设备和操作系统
-    // var system = {
-    //   win: false,
-    //   mac: false,
-    //   xll: false,
-    //   ipad:false
-    // };
-    //     //检测平台
-    //     var p = navigator.platform;
-    //     console.log(p);
-    //     system.win = p.indexOf("Win") == 0;
-    //     system.mac = p.indexOf("Mac") == 0;
-    //     system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-    //     system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
-    //     //跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面
-    //     if (system.win || system.mac || system.xll||system.ipad) {
-    //       alert('Please use a mobile phone to access this page');
-    //       window.location.href = "http://www.google.com";
-    //     } 
+    //平台、设备和操作系统
+    var system = {
+      win: false,
+      mac: false,
+      xll: false,
+      ipad:false
+    };
+        //检测平台
+        var p = navigator.platform;
+        console.log(p);
+        system.win = p.indexOf("Win") == 0;
+        system.mac = p.indexOf("Mac") == 0;
+        system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+        system.ipad = (navigator.userAgent.match(/iPad/i) != null)?true:false;
+        //跳转语句，如果是手机访问就自动跳转到wap.baidu.com页面
+        if (system.win || system.mac || system.xll||system.ipad) {
+          alert('Please use a mobile phone to access this page');
+          window.location.href = "http://www.google.com";
+        } 
 
 
   });
